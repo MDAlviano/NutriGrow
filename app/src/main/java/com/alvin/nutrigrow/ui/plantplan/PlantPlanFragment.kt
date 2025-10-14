5c5982cce3570a60c2596e6841999d05e7015d6b
MDAlviano <mdalviano.21122@gmail.com>
2025-10-14 20:36:05 +0700

    refactor: change some style

--- app/src/main/java/com/alvin/nutrigrow/ui/plantplan/PlantPlanFragment.kt
+++ app/src/main/java/com/alvin/nutrigrow/ui/plantplan/PlantPlanFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.Navigation
 import com.alvin.nutrigrow.R
 import com.alvin.nutrigrow.databinding.FragmentPlantPlanBinding
@@ -26,14 +27,28 @@ class PlantPlanFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.fabCreatePlantPlan.setOnClickListener {
-            Navigation.createNavigateOnClickListener(R.id.action_navigation_plantplan_to_navigation_create_plant_plan_activity2)
-        }
+        setListener()
+        setSpinnerItems()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun setListener() {
+        binding.fabCreatePlantPlan.setOnClickListener {
+            Navigation.createNavigateOnClickListener(R.id.action_navigation_plantplan_to_navigation_create_plant_plan_activity2)
+        }
+    }
+
+    private fun setSpinnerItems() {
+        val spinner = binding.spMedium
+        val mediums = listOf<String>("Hidroponik", "Tanah")
+        val adapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, mediums)
+
+        spinner.adapter = adapter
     }
 
 }
